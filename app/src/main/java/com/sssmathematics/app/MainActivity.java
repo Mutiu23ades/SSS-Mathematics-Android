383a0f06aeb3a0cf2c6fbc6aba232b71f609ea47
Mutiu23ades <admurtanob2357@gmail.com>
2026-09-03 16:51:17 +0100

    Refactor WebView initialization in MainActivity

--- app/src/main/java/com/sssmathematics/app/MainActivity.java
+++ app/src/main/java/com/sssmathematics/app/MainActivity.java
@@ -1,39 +1,26 @@
-
 package com.sssmathematics.app;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
 
-    private WebView webView;
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    WebView webView = new WebView(this);
 
-        webView = new WebView(this);
+    webView.getSettings().setJavaScriptEnabled(true);
+    webView.getSettings().setDomStorageEnabled(true);
 
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setDomStorageEnabled(true);
+    webView.setWebViewClient(new WebViewClient());
 
-        webView.setWebViewClient(new WebViewClient());
+    setContentView(webView);
 
-        webView.loadUrl("file:///android_asset/index.html");
+    webView.loadUrl("file:///android_asset/index.html");
+}
 
-        setContentView(webView);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
-    }
 }
